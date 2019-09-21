8fe82155762c9b3985af7e0a06a2bab2b42faae1
Matt Donaldson <matthewdonaldson9@gmail.com>
2019-09-21 17:28:15 -0400

    continued styling, some assets added

---
+++ script.js
@@ -0,0 +1,11 @@
+// const navScroll = document.getElementById("headerNav");
+// window.onscroll = function() {
+//   // "use strict";
+//   if (document.body.scrollTop >= 200) {
+//     navScroll.classList.add("headerNavScrolled");
+//     navScroll.classList.remove("headerNav");
+//   } else {
+//     navScroll.classList.add("headerNav");
+//     navScroll.classList.remove("headerNavScrolled");
+//   }
+// };
